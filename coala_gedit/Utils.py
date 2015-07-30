5da3d1449dd2c029abe9554e0015d632b28eda4a
AbdealiJK <abdealikothari@gmail.com>
2015-07-30 17:29:56 +0530

    coala_gedit: Add Utils

---
+++ coala_gedit/Utils.py
@@ -0,0 +1,10 @@
+"""
+Holds various common functions and variables which will be useful in general
+by the other classes.
+"""
+
+
+# COALA_KEY is to specify which names for variables in gedit's plugin system
+# which have been created by coala. (so that they are not confused with inbuilt
+# names or names from other plugins)
+COALA_KEY = "coala-gedit"
